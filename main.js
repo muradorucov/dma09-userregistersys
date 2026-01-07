96b4776199deb32f20f8f09ff52787d37a630175
muradorucov <murad.po@code.edu.az>
2026-01-07 06:01:20 +0400

    finish

--- main.js
+++ main.js
@@ -9,8 +9,25 @@
 
 const formElem = document.querySelector("form");
 const tbody = document.querySelector("table tbody")
+const searchInput = document.getElementById("searchInput");
+const sortBtns = document.querySelectorAll(".sortBtn");
+const totalResult = document.getElementById("totalResult");
+const paginationBtns = document.getElementById("btns")
+let lastList;
+
+
 let users = [];
+let localData = JSON.parse(localStorage.getItem("users"));
+if (localData) {
+  users = localData;
+  usersRender()
+}
+
 let id = 0;
+let localId = JSON.parse(localStorage.getItem("id"))
+if (localId) {
+  id = localId;
+}
 let isEdit = false;
 let globalId;
 
@@ -20,10 +37,6 @@ formElem.addEventListener("submit", (e) => {
   const fullName = formElem.fullName.value.trim()
   const email = formElem.email.value.trim()
   const password = formElem.password.value.trim()
-
-  console.log(formElem);
-  console.log(formElem.fullName);
-
 
   if (!isEdit) {
     const userExist = users.find(user => user.email === email);
@@ -39,6 +52,7 @@ formElem.addEventListener("submit", (e) => {
         password
       }
     )
+    localStorage.setItem("id", id)
   } else {
     const userExist = users.find(user => user.id === globalId);
     userExist.fullName = fullName;
@@ -46,12 +60,43 @@ formElem.addEventListener("submit", (e) => {
     userExist.password = password;
     isEdit = false;
   }
-  // ********
+
+  localStorage.setItem("users", JSON.stringify(users))
   usersRender();
   formElem.reset()
 })
+searchInput.addEventListener("input", () => {
+  const val = searchInput.value.trim().toLowerCase();
+  const filteredUsers = users.filter(user => user.fullName.toLowerCase().includes(val) ||
+    user.email.toLowerCase().includes(val))
+  usersRender(filteredUsers)
+})
+let isSorted = {
+  fullName: false,
+  email: false
+};
+sortBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    console.log("Berfore:", isSorted);
+    if (isSorted[btn.id]) {
+      users.sort((a, b) => {
+        return b[btn.id].localeCompare(a[btn.id])
+      })
+      isSorted[btn.id] = false
+    } else {
+      users.sort((a, b) => {
+        return a[btn.id].localeCompare(b[btn.id])
+      })
+      isSorted[btn.id] = true
+    }
+    console.log("After:", isSorted);
+    usersRender()
+  })
+})
+
 function deletUser(id) {
   users = users.filter(user => user.id !== id);
+  localStorage.setItem("users", JSON.stringify(users))
   usersRender();
 }
 
@@ -64,9 +109,11 @@ function editUser(id) {
   formElem.password.value = foundUser.password;
 }
 
-function usersRender() {
-  tbody.innerHTML = ""
-  users.forEach(user => {
+function usersRender(list = users, start = 0, end = 10) {
+  lastList = list;
+  totalResult.innerText = users.length;
+  tbody.innerHTML = "";
+  list.slice(start, end).forEach(user => {
     tbody.innerHTML += `
             <tr class="hover:bg-gray-50">
               <td class="p-3 border">${user.id}</td>
@@ -88,4 +135,25 @@ function usersRender() {
             </tr>
     `
   })
+  generateBtn(list)
 }
+
+
+function generateBtn(list) {
+  let totalPage = Math.ceil(list.length / 10);
+  paginationBtns.innerHTML = "";
+  for (let i = 1; i <= totalPage; i++) {
+    paginationBtns.innerHTML += `
+    <button class="w-10 h-10 bg-[blueviolet] text-white cursor-pointer"
+    onclick="getPagination(${i})">${i}</button>
+    `
+  }
+}
+
+
+function getPagination(num) {
+  let start = (num - 1) * 10;
+  let end = num * 10;
+  usersRender(lastList, start, end);
+}
+// pagination generate => limit 10, len 16, page 2
